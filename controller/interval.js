e63416430d7fe66ebc2ce783a1321809bf4b70dc
YunkoRoman <romanyunko1996@gmail.com>
2019-07-20 21:13:26 +0300

    optimization

--- controller/interval.js
+++ controller/interval.js
@@ -6,7 +6,13 @@ module.exports = setInterval(async function () {
     // console.log(date);
     await pool.query(`SELECT * FROM chat WHERE checked = ${false} AND date <= CURRENT_TIMESTAMP() `, (err, result) =>{
         for (const res of result) {
-            bot.sendMessage(res.chatId, res.text)
-        pool.query(`UPDATE chat SET checked = true  WHERE id = ${res.id}`)}
+            bot.sendMessage(res.chatId,`id нагадування: ${res.id}; Текст нагадуваня: ${res.text}` );
+            bot.sendMessage(res.chatId, '/done');
+            bot.sendMessage(res.chatId, ' Якщо ви хочете відтерінувати дане нагадування то введіть: /postpone <id нагадування>^<date>');
+
+            bot.onText(/\/done/, function (msg, match) {
+                pool.query(`UPDATE chat SET checked = true  WHERE id = ${res.id}`)
+            } )
+        }
     })
-}, 5000);
+}, 10000);
